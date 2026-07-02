ab1733e8346abd36360e4fab0811330f755dc652
arjyaaryanroutray-cloud <arjyaaryanroutray@gmail.com>
2026-07-02 08:12:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,4 +13,5 @@ app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+if __name__ == "__main__":
+    app.run_polling(drop_pending_updates=True)
